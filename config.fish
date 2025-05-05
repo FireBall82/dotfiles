2895599ae3ee291c5dc89714aa6a4ddc6e1f56d7
FireBall82 <95376940+FireBall82@users.noreply.github.com>
2025-05-05 19:11:03 +0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,12 @@
+function fish_greeting                                            
+    echo "aww shit here we go again"
+end
+function fish_prompt
+    echo ' -'$PWD '->'
+end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fastfetch
+alias config='cd / && cd etc/nixos && sudo nvim configuration.nix'
+alias rebuild='sudo nixos-rebuild switch'
