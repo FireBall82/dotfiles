bc1619b93dd4e2ad9087d44b14923bc1578988dd
FireBall82 <95376940+FireBall82@users.noreply.github.com>
2025-05-09 17:37:03 +0300

    Add files via upload

--- config.fish
+++ config.fish
@@ -7,6 +7,9 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+
 fastfetch
 alias config='cd / && cd etc/nixos && sudo nvim configuration.nix'
 alias rebuild='sudo nixos-rebuild switch'
