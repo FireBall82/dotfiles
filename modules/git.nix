015fa7464c41d3de5394abca9a2e4975f4f7f0e3
FireBall82 <romennegrik82@gmail.com>
2026-01-22 14:59:57 +0200

    sdsd

--- modules/git.nix
+++ modules/git.nix
@@ -7,6 +7,10 @@
         userName = "FireBall82";
         userEmail = "romennegrik82@gmail.com";
       };
+      extraConfig = {
+        init.defaultBranch = "main"; # This line
+        push.autoSetupRemote = true;
+      };
     };
   };
   home.packages = with pkgs; [
