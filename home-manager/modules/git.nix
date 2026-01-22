388989526f3ad506a62d5becb0189e6028951a4c
FireBall82 <95376940+FireBall82@users.noreply.github.com>
2026-01-22 15:29:04 +0200

    Remove extra Git configuration
    Removed extra Git configuration for default branch and remote setup.

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -7,10 +7,6 @@
         userName = "FireBall82";
         userEmail = "romennegrik82@gmail.com";
       };
-      extraConfig = {
-        init.defaultBranch = "main"; # This line
-        push.autoSetupRemote = true;
-      };
     };
   };
   home.packages = with pkgs; [
